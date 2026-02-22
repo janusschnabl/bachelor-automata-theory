e4ee47110dd88519d05ebee9860d4b2ac880a971
janusschnabl <s235265@dtu.dk>
2026-02-22 18:49:53 +0100

    publish

--- apps/inspectify/src/lib/components/Network.svelte
+++ apps/inspectify/src/lib/components/Network.svelte
@@ -8,7 +8,112 @@
   }
 
   let { dot }: Props = $props();
+  let container: HTMLDivElement | undefined = $state();<script lang="ts">
+  import { mirage } from 'ayu';
+  import { onMount } from 'svelte';
+  import type { Network } from 'vis-network/esnext';
+
+  interface Props {
+    dot: string;
+  }
+
+  let { dot }: Props = $props();
+
   let container: HTMLDivElement | undefined = $state();
+  let network: Network | undefined = $state();
+
+  let redraw = $derived(async () => {
+    let preDot = dot;
+    const vis = await import('vis-network/esnext');
+    if (preDot != dot) return;
+    const data = vis.parseDOTNetwork(dot);
+
+    // EVT TODO. LAV EN CUSTOM SHAPE HERUNDER MED DOBBELTCIRKEL
+    data.nodes.forEach((node: any) => {
+      if (node.isAccepting) {
+        node.shape = 'circle';
+        node.size = 28;
+        node.borderWidth = 2;
+
+        node.color = {
+          background: mirage.ui.fg.hex(),
+          border: '#85CC95',
+          highlight: mirage.ui.fg.brighten(1).hex(),
+        };
+        node.shadow = {
+          enabled: true,
+          color: '#85CC95',
+          size: 25,
+          x: 0,
+          y: 0,
+        };
+      }
+    });
+
+    if (network) {
+      network.setData(data);
+    } else {
+      if (!container) return;
+
+      network = new vis.Network(container, data, {
+        // interaction: { zoomView: false },
+        nodes: {
+          color: {
+            background: mirage.ui.fg.hex(),
+            border: mirage.ui.fg.hex(),
+            highlight: mirage.ui.fg.brighten(1).hex(),
+            // background: '#666666',
+            // border: '#8080a0',
+            // highlight: '#80a0ff',
+          },
+          font: {
+            color: 'white',
+          },
+          borderWidth: 1,
+          shape: 'circle',
+          size: 30,
+        },
+        edges: {
+          // color: '#D0D0FF',
+          color: mirage.syntax.constant.hex(),
+          font: {
+            color: 'white',
+            strokeColor: '#200020',
+            face: 'Menlo, Monaco, "Courier New", monospace',
+          },
+        },
+        autoResize: true,
+      });
+    }
+  });
+
+  onMount(() => {
+    if (!container) return;
+    const observer = new ResizeObserver(() => {
+      requestAnimationFrame(() => {
+        if (network) {
+          network.fit({ animation: false, maxZoomLevel: 20 });
+          network.redraw();
+        }
+      });
+    });
+    observer.observe(container);
+    return () => observer?.disconnect();
+  });
+
+  onMount(() => {
+    redraw();
+  });
+
+  $effect(() => {
+    dot && network && redraw();
+  });
+</script>
+
+<div class="relative h-full w-full">
+  <div class="absolute inset-0" bind:this={container}></div>
+</div>
+
   let network: Network | undefined = $state();
 
   let redraw = $derived(async () => {
